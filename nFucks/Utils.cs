994c8b1cc5682014f9618aba0ec37f165d2b5f07
Some1-One <42841016+Some1-One@users.noreply.github.com>
2018-09-02 22:20:53 +0430

    Update Utils.cs
    Reverted to original format

--- nFucks/Utils.cs
+++ nFucks/Utils.cs
@@ -18,23 +18,40 @@ namespace nFucks {
         public static ITermColor getITermColor (int color) {
             switch (color) {
                 switch (color) {
-                case  0: return new StaticSingleTermColorProvider(System.ConsoleColor.White);
-                case  1: return new StaticSingleTermColorProvider(System.ConsoleColor.Black);
-                case  2: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkBlue);
-                case  3: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkGreen);
-                case  4: return new StaticSingleTermColorProvider(System.ConsoleColor.Red);
-                case  5: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkRed);
-                case  6: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkMagenta);
-                case  7: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkYellow);
-                case  8: return new StaticSingleTermColorProvider(System.ConsoleColor.Yellow);
-                case  9: return new StaticSingleTermColorProvider(System.ConsoleColor.Green);
-                case 10: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkCyan);
-                case 11: return new StaticSingleTermColorProvider(System.ConsoleColor.Cyan);
-                case 12: return new StaticSingleTermColorProvider(System.ConsoleColor.Blue);
-                case 13: return new StaticSingleTermColorProvider(System.ConsoleColor.Magenta);
-                case 14: return new StaticSingleTermColorProvider(System.ConsoleColor.DarkGray);
-                case 15: return new StaticSingleTermColorProvider(System.ConsoleColor.Gray);
-                default: return BasicColor.Default; // unknown color
+                case  0: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.White);
+                case  1: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Black);
+                case  2: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkBlue);
+                case  3: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkGreen);
+                case  4: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Red);
+                case  5: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkRed);
+                case  6: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkMagenta);
+                case  7: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkYellow);
+                case  8: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Yellow);
+                case  9: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Green);
+                case 10: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkCyan);
+                case 11: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Cyan);
+                case 12: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Blue);
+                case 13: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Magenta);
+                case 14: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.DarkGray);
+                case 15: 
+                        return new StaticSingleTermColorProvider(System.ConsoleColor.Gray);
+                default: 
+                        return BasicColor.Default; // unknown color
             }
         }
     }
